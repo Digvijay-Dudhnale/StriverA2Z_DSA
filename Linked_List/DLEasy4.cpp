6de0c1379e4ac32ff424d8a771a88e6754efaacb
Digvijay Dudhnale <dlearner421@gmail.com>
2024-08-03 20:53:10 +0530

    3rdaug2024

---
+++ Linked_List/DLEasy4.cpp
@@ -0,0 +1,140 @@
+// Given a doubly linked list of n elements. Your task is to reverse the doubly linked list in-place.
+
+// Expected Time Complexity: O(n).
+// Expected Auxiliary Space: O(1).
+// Constraints:
+// 1 <= number of nodes <= 104
+// 0 <= value of nodes <= 104
+
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node * next;
+    Node * prev;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+        prev=NULL;
+    }
+        
+};
+
+Node *newNode(int data)
+{
+    Node *temp=new Node(data);
+    
+    return temp;
+}
+
+void displayList(Node *head)
+{
+    //Head to Tail
+    while(head->next)
+    {
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+    cout<<head->data;
+    
+    
+    
+}
+
+int getLength(Node * head)
+{
+    Node *temp=head;
+    
+    int count=0;
+    while(temp->next!=head)
+    {
+        count++;
+        temp=temp->next;
+    }
+    return count+1;
+}
+
+bool verify(Node* head)
+{
+    int fl=0;
+    int bl=0;
+    
+    Node *temp=head;
+    
+    while(temp->next)
+    {
+        temp=temp->next;
+        fl++;
+    }
+    
+    while(temp->prev)
+    {
+        temp=temp->prev;
+        bl++;
+    }
+    
+    return fl==bl;
+}
+
+class Solution
+{
+    public:
+    Node* reverseDLL(Node * head)
+    {
+        //Your code here
+        Node *nextNode ;
+        while(head->next){
+            nextNode = head->prev;
+            head->prev = head->next;
+            head->next = nextNode;
+            head = head->prev;
+        }
+        head->next = head->prev;
+        head->prev = NULL;
+        
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    Node *head=NULL, *tail=NULL;
+        int x;
+	    cin>>x;
+	    head = newNode(x);
+	    tail = head;
+	    
+	    for(int i=0;i<n - 1;i++)
+	    {
+	        cin>>x;
+	        Node* temp=newNode(x);
+	        tail->next=temp;
+	        temp->prev= tail;
+	        tail = temp;
+	    }
+	    Solution ob;
+	    head=ob.reverseDLL(head);
+	    
+	    
+	    if(verify(head))
+	    displayList(head);
+	    else
+	    cout<<"Your pointers are not correctly connected";
+ 
+	    cout<<endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
